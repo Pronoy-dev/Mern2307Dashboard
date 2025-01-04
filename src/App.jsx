bf17bfafa8d06f6438b3c17b4b9f490c72616394
taufik69 <taufik35-2915@diu.edu.bd>
2025-01-04 15:45:12 +0600

    dashboard scaffholding

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { Button } from "@material-tailwind/react";
+
+function App() {
+  return (
+    <div className="bg-red-200">
+      <Button>sadlkjf</Button>
+    </div>
+  );
+}
+
+export default App;
